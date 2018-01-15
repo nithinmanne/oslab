3f21153709d9d5fa1b94de1f3a4e5459e437fac4
Naga Nithin Manne <nithinmanne@gmail.com>
2018-01-15 16:55:30 +0530

    Finished B,C,D,E

--- 1/Ass1_51_1a.cpp
+++ 1/Ass1_51_1a.cpp
@@ -10,18 +10,24 @@ int cmp(const void *a, const void *b) {
 #define COUNT 100
 
 int main() {
-    pid_t pid[3] = {0, 0, 0}; int pip[3][2];
-    pipe(pip[0]); pipe(pip[1]); pipe(pip[2]);
-    int a_pip = pip[0][1]; pid[0] = fork();
+    pid_t pid[3] = {0, 0, 0};
+    int pip[3][2];
+    pipe(pip[0]);
+    pipe(pip[1]);pipe(pip[2]);
+    int a_pip = pip[0][1];
+    pid[0] = fork();
     if(pid[0]!=0) {
-    a_pip = pip[1][1]; pid[1] = fork();
+        a_pip = pip[1][1];
+        pid[1] = fork();
     }
-	if(pid[1]!=0) {
-    a_pip = pip[2][1]; pid[2] = fork();
+    if(pid[1]!=0) {
+        a_pip = pip[2][1];
+        pid[2] = fork();
     }
 	if(pid[2]==0) {
 		int ar[COUNT];
-        minstd_rand rand; rand.seed((unsigned)a_pip);
+        minstd_rand rand;
+        rand.seed((unsigned)a_pip);
 		for (int &i : ar) i = (int)rand();
 		qsort(ar, COUNT, sizeof(int), cmp);
 		for (int &i : ar) write(a_pip, &i, sizeof(int));
@@ -30,11 +36,11 @@ int main() {
         int ar[3*COUNT], r[3]={1, 1, 1}, c[3]={0, 0, 0}, t[3], cmn = -1;
 		for (int &i : ar) {
 			for(int j=0;j<3;j++)
-        if(r[j]==1&&c[j]<COUNT) {
-          read(pip[j][0], &t[j], sizeof(int));
-          r[j] = 0; c[j]++;
-          if(cmn==-1||t[j]<t[cmn]) cmn = j;
-        }
+                if(r[j]==1&&c[j]<COUNT) {
+                  read(pip[j][0], &t[j], sizeof(int));
+                  r[j] = 0; c[j]++;
+                  if(cmn==-1||t[j]<t[cmn]) cmn = j;
+                }
             i = t[cmn]; r[cmn] = 1;
             bool u1 = c[(cmn+1)%3]<COUNT||r[(cmn+1)%3]==0, u2 = c[(cmn+2)%3]<COUNT||r[(cmn+2)%3]==0;
             if(u1&&u2)
--- 2/Ass2_51.cpp
+++ 2/Ass2_51.cpp
@@ -1,28 +1,108 @@
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/fcntl.h>
 
 using namespace std;
 
 #define EXIT "quit"
+#define W_MODE "Error! Invalid Mode!\n"
+#define W_COMM "Error! Invalid Command!\n"
+
+bool invalid(const string &mode) {
+    if(mode.length()!=1) return true;
+    if(mode[0]=='A') return false;
+    if(mode[0]=='B') return false;
+    if(mode[0]=='C') return false;
+    if(mode[0]=='D') return false;
+    if(mode[0]=='E') return false;
+    if(mode[0]=='F') return false;
+    return mode[0] != 'G';
+}
+
+int parse_red(const string &comm, char sep, string *out) {
+    long loc = comm.find(sep);
+    if(loc==-1) return 1;
+    out[0] = comm.substr(0, (unsigned)loc);
+    out[1] = comm.substr((unsigned)loc+1, comm.length());
+    unsigned long fns = out[1].find_first_not_of(' ');
+    out[1] = out[1].substr(fns, out[1].length());
+    return 0;
+}
 
 int main()
 {
-    string comm;
+    string mode, comm, comm_s[2];
+    int status, x;
     while(true) {
-        cout<<"A. Run an internal command\n";
-        cout<<"B. Run an external command\n";
-        cout<<"C. Run an external command by redirecting standard input from a file\n";
-        cout<<"D. Run an external command by redirecting standard output to a file\n";
-        cout<<"E. Run an external command in the background\n";
-        cout<<"F. Run several external commands in the pipe mode\n";
-        cout<<"G. Quit the shell\n";
+        if(comm!=W_MODE && comm!=W_COMM) {
+            cout << "A. Run an internal command\n";
+            cout << "B. Run an external command\n";
+            cout << "C. Run an external command by redirecting standard input from a file\n";
+            cout << "D. Run an external command by redirecting standard output to a file\n";
+            cout << "E. Run an external command in the background\n";
+            cout << "F. Run several external commands in the pipe mode\n";
+            cout << "G. Quit the shell\n";
+        }
+        if(comm!=W_COMM) {
+            cout << "Enter Mode: ";
+            getline(cin, mode);
+            if (invalid(mode)) {
+                cout << W_MODE;
+                comm = W_MODE;
+                continue;
+            }
+            if (mode[0] == 'G')
+                break;
+        }
+        cout<<"Enter Command: ";
         getline(cin, comm);
-        if(comm==EXIT) break;
-        int x = fork();
-        chdir("dfs");
-        int status;
-        if(x==0) execlp("sh", "sh", "-c", comm.c_str(), nullptr);
-        else wait(&status);
+        switch(mode[0]) {
+            case 'A':
+                break;
+            case 'B':
+                x = fork();
+                if(x==0) execlp("sh", "sh", "-c", comm.c_str(), nullptr);
+                else wait(&status);
+                break;
+            case 'C':
+                parse_red(comm, '<', comm_s);
+                x = fork();
+                if(x==0) {
+                    fclose(stdin);
+                    dup(open(comm_s[1].c_str(), O_RDONLY));
+                    execlp("sh", "sh", "-c", comm_s[0].c_str(), nullptr);
+                }
+                else wait(&status);
+                break;
+            case 'D':
+                parse_red(comm, '>', comm_s);
+                x = fork();
+                if(x==0) {
+                    fclose(stdout);
+                    remove(comm_s[1].c_str());
+                    dup(open(comm_s[1].c_str(), O_CREAT | O_WRONLY));
+                    execlp("sh", "sh", "-c", comm_s[0].c_str(), nullptr);
+                }
+                else wait(&status);
+                break;
+            case 'E':
+                x = (int)comm.find_last_not_of(' ');
+                if(comm[x]!='&') {
+                    cout<<W_COMM<<"Must end with &\n";
+                    comm = W_COMM;
+                }
+                else {
+                    comm = comm.substr(0, (unsigned)x);
+                    cout<<comm;
+                    x = fork();
+                    if (x == 0) execlp("sh", "sh", "-c", comm.c_str(), nullptr);
+                }
+                break;
+            case 'F':
+                break;
+            default:
+                continue;
+        }
     }
 }
